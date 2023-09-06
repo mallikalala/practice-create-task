7bc8c7d107c667fbf0d8f9f08398c4a0056b3f59
Mallika Sheshadri <mallika.n.sheshadri@gmail.com>
2023-09-06 21:10:10 +0000

    addItem

--- git-practice.py
+++ git-practice.py
@@ -3,3 +3,15 @@ middle= "nayna"
 last= "sheshadri"
 print("my name is first", first, middle,
  last)
+
+toDoList = ["Math Homework", "Cook Dinner", "Fold Laundry"]
+
+def addItem(item):
+   toDoList.append(item)
+   return toDoList
+
+userAns = input("Do you want to add to your list or quit? A/Q")
+while userAns == "A":
+   item = input("What item do you want to add?")
+   addItem(item)
+   userAns = input("Do you want to add to your list or quit? A/Q")
